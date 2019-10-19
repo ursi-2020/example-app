aa36fc5273339dc88ccfbbf4c211264fa74497a2
Alexandre Jacquot <alexandre.jacquot@epita.fr>
2019-10-19 19:29:20 +0200

    Added custom.env + moved register in apps.py

--- application/djangoapp/apps.py
+++ application/djangoapp/apps.py
@@ -1,5 +1,14 @@
+import os
+
 from django.apps import AppConfig
+from apipkg import api_manager as api
+
+myappurl = "http://localhost:" + os.environ["WEBSERVER_PORT"]
 
 
 class ApplicationConfig(AppConfig):
     name = 'application.djangoapp'
+
+    def ready(self):
+        api.unregister(os.environ['DJANGO_APP_NAME'])
+        api.register(myappurl, os.environ['DJANGO_APP_NAME'])
